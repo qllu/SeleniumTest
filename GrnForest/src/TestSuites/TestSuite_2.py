f1ef89789830c7286a4fb1946f80e3b77ea8ab6f
qllu <rikufly@126.com>
2015-10-21 22:28:51 +0800

    Modify TestSuite

--- GrnForest/src/TestSuites/TestSuite_2.py
+++ GrnForest/src/TestSuites/TestSuite_2.py
@@ -2,29 +2,34 @@
 '''
 Created on 2015年9月2日
 @author: QLLU
+用来执行TestCases目录中所有包含以QT开头的测试用例
+
 '''
 
 import HTMLTestRunner
 import unittest, os, sys, time
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
 sys.path.append("..")
 sys.path.append(os.getcwd()+"src/")
 
 #引用测试用例文件
-from TestCases import QT_login_grn
-from TestCases import QT_Space_create_space_category
-from TestCases import QT_Space_create_public_space
-from TestCases import QT_Space_create_private_space
+cases_direc = '../TestCases/'
+def suite1():
+    suite = unittest.TestSuite()
+    # 定义discover方法
+    discover = unittest.defaultTestLoader.discover(
+        cases_direc, pattern='QT_*.py', top_level_dir=None)
 
+    # discover方法筛选出用例，循环添加到测试套件中
+    for test_suite in discover:
+        for test_cases in test_suite:
+            suite.addTests(test_cases)
+            # print suite
+    return suite
 
-suite = unittest.TestSuite()
-
-# 文件名.类名
-# suite.addTest(unittest.makeSuite(QT_login_grn.LoginGrn))
-# suite.addTest(unittest.makeSuite(QT_Space_create_space_category.CreateSpaceCategory))
-suite.addTest(unittest.makeSuite(QT_Space_create_public_space.CreatePublicSpace))
-suite.addTest(unittest.makeSuite(QT_Space_create_private_space.CreatePrivateSpace))
+alltests = suite1()
 
 now = time.strftime("%Y%m%dT%H%M%S_",time.localtime(time.time()))
 filename = '../Report/' + now + 'result.html' #测试报告路径
@@ -35,9 +40,5 @@ fp = file(filename, 'wb')
 # 添加测试报告
 runner = HTMLTestRunner.HTMLTestRunner(stream = fp,title = 'Result',
                                        description = 'Test_Result：')
-runner.run(suite)
+runner.run(alltests)
 
-
-# 运行测试用例集
-# runner = unittest.TextTestRunner()
-# runner.run(suite)
--- GrnForest/src/TestSuites/TestSuite_3.py
+++ GrnForest/src/TestSuites/TestSuite_3.py
@@ -2,6 +2,8 @@
 '''
 Created on 2015年9月2日
 @author: QLLU
+用来执行测试用例中的具体方法
+
 '''
 
 import HTMLTestRunner
@@ -10,34 +12,45 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 sys.path.append("..")
 sys.path.append(os.getcwd()+"src/")
+sys.path.append(os.getcwd()+"/PythonTest/src/")
 
 #引用测试用例文件
-from TestCases import QT_login_grn
-from TestCases import QT_Space_create_space_category
-from TestCases import QT_Space_create_public_space
-from TestCases import QT_Space_create_private_space
+# from TestCases import QT_login_grn
+from TestCases.QT_Space_create_space_category import CreateSpaceCategory
+from TestCases.QT_Space_create_public_space import CreatePublicSpace
+from TestCases.QT_Space_create_private_space import CreatePrivateSpace
 
 
-suite = unittest.TestSuite()
+class TestSuite_3():
 
-# 文件名.类名
-# suite.addTest(unittest.makeSuite(QT_login_grn.LoginGrn))
-# suite.addTest(unittest.makeSuite(QT_Space_create_space_category.CreateSpaceCategory))
-suite.addTest(unittest.makeSuite(QT_Space_create_public_space.CreatePublicSpace))
-suite.addTest(unittest.makeSuite(QT_Space_create_private_space.CreatePrivateSpace))
+    def test(self):
 
-now = time.strftime("%Y%m%dT%H%M%S_",time.localtime(time.time()))
-filename = '../Report/' + now + 'result.html' #测试报告路径
+        if __name__ == '__main__':
+            # 构造测试集
+            suite = unittest.TestSuite()
+            # suite.addTest(unittest.makeSuite(QT_login_grn.LoginGrn) #方法2，文件名.类名
+            # suite.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(TestSuite_1))
 
-print filename
-fp = file(filename, 'wb')
+            suite.addTest(CreateSpaceCategory('test_create_space_category'))
+            suite.addTest(CreatePublicSpace('test1_create_public_space'))
+            suite.addTest(CreatePrivateSpace('test1_create_private_space'))
 
-# 添加测试报告
-runner = HTMLTestRunner.HTMLTestRunner(stream = fp,title = 'Result',
-                                       description = 'Test_Result：')
-runner.run(suite)
+            now = time.strftime("%Y%m%dT%H%M%S_",time.localtime(time.time()))
+            filename = '../Report/' + now + 'result.html' #测试报告路径
+
+            print filename
+            fp = file(filename, 'wb')
+
+            # 添加测试报告
+            runner = HTMLTestRunner.HTMLTestRunner(stream = fp,title = 'Result',
+                                                   description = 'Test_Result：')
+            runner.run(suite)
+
+            # 运行测试用例集
+            # runner = unittest.TextTestRunner()
+            # runner.run(suite)
+
+if __name__ == "__main__":
+    TestSuite_1().test()
 
 
-# 运行测试用例集
-# runner = unittest.TextTestRunner()
-# runner.run(suite)
