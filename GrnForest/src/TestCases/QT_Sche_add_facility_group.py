2512afceb59c3f2dc0999ca606e3a252527e5452
qllu <rikufly@126.com>
2015-11-16 18:22:30 +0800

    update

--- GrnForest/src/TestCases/QT_Sche_add_facility_group.py
+++ GrnForest/src/TestCases/QT_Sche_add_facility_group.py
@@ -17,6 +17,49 @@ from CommonFunction.DataReader import DataReader
 from CommonFunction.Operations import Operations
 from CommonFunction.WebDriver import WebDriver
 
+def add_facility_group():
+    global group_url, group_detail_url
+    # 进入日程安排系统后台，添加设备组
+    garoon_url = WebDriver().testurl("qatest01") + "/g/system/application_list.csp?app_id="
+    WebDriver().geturl(garoon_url)
+    time.sleep(1)
+    WebDriver().click("byid", "schedule")
+    WebDriver().click("byid", "schedule/system/facility_group")
+    WebDriver().click("byid", "schedule/system/facility_group_add")
+    time.sleep(1)
+    WebDriver().input("byname", "facilitygroupName-def", "fac_group1")
+    WebDriver().input("byname", "memo", "this is a facility group")
+    WebDriver().click("byid", "facility_group_add_submit")
+    WebDriver().click("bylink", "fac_group1")
+    group_url = WebDriver().currenturl()
+    # 点击设备组进入详情
+    WebDriver().click("byxpath", "//div[2]/span/a")
+    time.sleep(1)
+    group_detail_url = WebDriver().currenturl()
+
+def add_facility():
+    global fac_detail_url
+    WebDriver().geturl(group_url)
+    time.sleep(2)
+    WebDriver().click("byid", "schedule/system/facility_add")
+    WebDriver().input("byname", "facilityName-def", "fac1")
+    WebDriver().input("byname", "facility_code", "fac1_code")
+    WebDriver().input("byname", "memo", "this is a facility")
+    WebDriver().click("byid", "facility_add_submit")
+    # 点击进入设备详情
+    WebDriver().click("byxpath", "//td[2]/span/a")
+    fac_detail_url = WebDriver().currenturl()
+
+def del_fac():
+    WebDriver().geturl(fac_detail_url)
+    time.sleep(2)
+    WebDriver().click("byxpath", "//span[2]/span[2]/span/a")
+    WebDriver().click("bycss", "input.margin")
+    # 删除设备组
+    WebDriver().geturl(group_detail_url)
+    time.sleep(2)
+    WebDriver().click("byxpath", "//span[2]/span[2]/span/a")
+    WebDriver().click("bycss", "input.margin")
 
 class AddFacilityGroup(unittest.TestCase):
 
@@ -30,59 +73,24 @@ class AddFacilityGroup(unittest.TestCase):
         Operations().login(dataoper.readxml('login', 0, 'username'),
                               dataoper.readxml('login', 0, 'password'))
         time.sleep(2)
-        # 进入日程安排系统后台
-        garoon_url = WebDriver().testurl("qatest01") + "/g/system/application_list.csp?app_id="
-        WebDriver().geturl(garoon_url)
-        time.sleep(1)
-        WebDriver().click("byid", "schedule")
-        WebDriver().click("byid", "schedule/system/facility_group")
-        WebDriver().click("byid", "schedule/system/facility_group_add")
-        time.sleep(1)
-        WebDriver().input("byname", "facilitygroupName-def", "fac_group1")
-        WebDriver().input("byname", "memo", "this is a facility group")
-        WebDriver().click("byid", "facility_group_add_submit")
-        # 点击设备组进入详情
-        WebDriver().click("bylink", "fac_group1")
-        group_url = WebDriver().currenturl()
-        WebDriver().click("byxpath", "//div[2]/span/a")
-        time.sleep(1)
+        add_facility_group()
         check1 = WebDriver().gettext("bycss", "td")
         check2 = WebDriver().gettext("byxpath", "//tr[4]/td")
         self.assertEqual(check1, "fac_group1"), "设备组名称验证失败"
         self.assertEqual(check2, "this is a facility group") ,"设备组备注验证失败"
-        group_detail_url = WebDriver().currenturl()
 
 
     def test2_add_facility(self):
-        global fac_detail_url
-        WebDriver().geturl(group_url)
-        time.sleep(2)
-        WebDriver().click("byid", "schedule/system/facility_add")
-        WebDriver().input("byname", "facilityName-def", "fac1")
-        WebDriver().input("byname", "facility_code", "fac1_code")
-        WebDriver().input("byname", "memo", "this is a facility")
-        WebDriver().click("byid", "facility_add_submit")
-        time.sleep(2)
-        # 点击进入设备详情
-        WebDriver().click("byxpath", "//td[2]/span/a")
+        add_facility()
         check3 = WebDriver().gettext("bycss", "td")
         self.assertEqual(check3, "fac1")
-        fac_detail_url = WebDriver().currenturl()
+
 
     @classmethod
     def tearDownClass(self):
         # 清空数据
         try:
-            # 删除设备
-            WebDriver().geturl(fac_detail_url)
-            time.sleep(2)
-            WebDriver().click("byxpath", "//span[2]/span[2]/span/a")
-            WebDriver().click("bycss", "input.margin")
-            # 删除设备组
-            WebDriver().geturl(group_detail_url)
-            time.sleep(2)
-            WebDriver().click("byxpath", "//span[2]/span[2]/span/a")
-            WebDriver().click("bycss", "input.margin")
+            del_fac()
         except Exception as msg:
             print msg, "数据不能正常清除"
         finally:
--- GrnForest/src/TestCases/QT_Sche_add_sche_with_users_facility.py
+++ GrnForest/src/TestCases/QT_Sche_add_sche_with_users_facility.py
@@ -15,7 +15,7 @@ sys.path.append(os.getcwd() + "/src/")
 from CommonFunction.DataReader import DataReader
 from CommonFunction.Operations import Operations
 from CommonFunction.WebDriver import WebDriver
-
+import QT_Sche_add_facility_group as addfac
 
 class AddSche(unittest.TestCase):
 
@@ -23,22 +23,56 @@ class AddSche(unittest.TestCase):
         WebDriver("open","firefox","local").open("qatest01")  # 打开浏览器，并打开forest
 
     def test1_add_sche_with_users_facility(self):
+        # dataoper = DataReader('QT_Sche_add_sche_with_users_facility.xml')
+        # Operations().login(dataoper.readxml('login', 0, 'username'),
+        #                       dataoper.readxml('login', 0, 'password'))
+        # addfac.add_facility_group()
+        # addfac.add_facility()
+        # Operations().logout()
+        # 使用u1操作
         dataoper = DataReader('QT_Sche_add_sche_with_users_facility.xml')
-        Operations().login(dataoper.readxml('login', 0, 'username'),
-                              dataoper.readxml('login', 0, 'password'))
-        time.sleep(2)
-        garoon_url = WebDriver().testurl("qatest01") + "/g/system/application_list.csp?app_id="
+        Operations().login(dataoper.readxml('sche', 0, 'username'),
+                              dataoper.readxml('sche', 0, 'password'))
+        garoon_url = WebDriver().testurl("qatest01") + "/g/schedule/index.csp?"
         WebDriver().geturl(garoon_url)
+        time.sleep(2)
+        WebDriver().click("byxpath", ".//*[@id='smart_main_menu_part']/span[1]/a")
+        time.sleep(2)
+        # 选择结束时间
+        WebDriver().click('byid', "time_selector")
+        WebDriver().click("byid", "time16")
+        # 输入标题
+        WebDriver().input('byname', "title", "sche01")
+        # 检索用户并添加
         time.sleep(1)
-        WebDriver().click("byid", "schedule")
-        WebDriver().click("byid", "schedule/system/common_set")
-        # 设置附件
-        print WebDriver().is_selected("byid", "allow_file_attachment")
+        WebDriver().input("byname", "keyword_CGID", "u2")
+        time.sleep(1)
+        WebDriver().click("byid", "searchbox-submit-users")
+        time.sleep(1)
+        WebDriver().click("bycss", "span.aButtonText-grn")
+        # 检索设备并添加
+        time.sleep(1)
+        WebDriver().input("byid", "facility_search_text", "fac1")
+        time.sleep(1)
+        WebDriver().click("byid", "searchbox-submit-facilities")
+        time.sleep(1)
+        WebDriver().click("byxpath", "//table[@id='main_table']/tbody/tr[4]/td/table/tbody/tr/td[2]/div/div/span/a/span[2]")
+        # 添加附件
+        upfile = os.path.abspath('../Attachement/cybozu.gif')
+        WebDriver().input("byid", "file_upload_", upfile)
+        time.sleep(1)
+        WebDriver().click("byid", "schedule_submit_button")
 
 
     def tearDown(self):
         # 清空数据
         pass
+        # try:
+        #     addfac.del_fac()
+        # except Exception as msg:
+        #     print msg, "数据不能正常清除"
+        # finally:
+        #     WebDriver().close()
 
 
 if __name__ == "__main__":
